db496f396b7230abc856de35930a5dc0ea32143c
Yat251 <yatharth85204@gmail.com>
2023-06-22 14:34:49 +0530

    rds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,17 +85,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'main1',
+#         'USER': 'main',
+#         'PASSWORD': 'imindian',
+#         'HOST': 'main1.cufxluinuzsz.us-east-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'edconclave',
-        'USER': 'yatharth',
-        'PASSWORD': 'TF27dreamon',
-        'HOST': 'edconclave.cvvgh2n9qf93.us-east-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
