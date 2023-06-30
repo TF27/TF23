8d13f5803ca737cbe0e1a0efde5e09918086abca
Aymaan Shahzad <22b1520@iitb.ac.in>
2023-06-30 21:15:48 +0530

    ResponsiveHamburgerMenuAdded

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,9 +7,8 @@ import './navbar.css';
 const Navbar = () => {
   const navigate = useNavigate();
   const { googleSignIn, user, logOut } = UserAuth();
-
   const handleGoogleSignIn = async () => {
-    try {
+    try{
       await googleSignIn();
     } catch (error) {
       console.log(error);
@@ -31,46 +30,68 @@ const Navbar = () => {
     name = username.split(' ')[0];
   }
 
+const openMenu = ()=>{
+  const navMenu = document.querySelector(".nav-menu").classList;
+  const hamburger = document.querySelector(".hamburger").classList;
+  hamburger.toggle("active");
+  navMenu.toggle("active")
+}
+
+
 const handleClick = (e) => {
-  const items = document.querySelectorAll('.item');
+  const items = document.querySelectorAll('.nav-link');
   items.forEach(item => {
     item.classList.remove('active');
   });
-
-  console.log(e.target, 'clicked');
-    e.target.classList.toggle('active')
+  
+    // e.target.classList.toggle('active')
 }
 
 return (
     <>
-      <div className="navbar" >
-        <div className="logo">
-          <a href="https://techfest.org/" target="_blank" rel="noopener noreferrer">
-            <img src='#' alt="Techfest, IIT Bombay" />
-          </a>
-        </div>
-        <div className="navbar-items">
-          <Link className="item active" to="/" onClick={handleClick}>
-            Home
-          </Link>
-          <Link className='item' to="/competitions" onClick={handleClick}>
-            Competitions
-          </Link>
-           
-          {user?.displayName ? (
-            <div>
-              <button onClick={handleSignOut} className="item1" onMouseEnter={(e)=> e.target.innerText = "Sign Out"} onMouseLeave={(e)=> e.target.innerText = name}>
-              {name}
+      <header>
+        <nav className="navbar curved" >
+          <span><a href="https://techfest.org/" className='nav-branding' target="_blank" rel="noopener noreferrer">
+              <img src='#' className='tflogo' alt="Techfest, IIT Bombay"/>
+            </a></span>
+            <ul className='nav-menu'>
+              <li className='nav-item'><Link className="nav-link" to="/" onClick={handleClick}>
+                Media
+              </Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/history" onClick={handleClick}>
+                History
+              </Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/lectures" onClick={handleClick}>
+                Lectures
+              </Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/exhibitions" onClick={handleClick}>
+                Exhibition
+              </Link></li>
+              <li className='nav-item'><Link className="nav-link" to="/competitions" onClick={handleClick}>
+                Competitions
+              </Link></li>      
+            </ul>     
+            {user?.displayName ? (
+              <div>
+                <button onClick={handleSignOut} className="item1" onMouseEnter={(e)=> e.target.innerText = "Sign Out"} onMouseLeave={(e)=> e.target.innerText = name}>
+                {name}
+                </button>
+                
+              </div>
+            ) : (
+              <button onClick={handleGoogleSignIn} className="item2">
+                Sign In
               </button>
-              
+            )}
+            <div onClick={openMenu} className="hamburger ">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span></span>
+              <span></span>
             </div>
-          ) : (
-            <button onClick={handleGoogleSignIn} className="item2">
-              Sign In
-            </button>
-          )}
-        </div>
-      </div>
+          </nav>
+      </header>
     </>
   );
 };
