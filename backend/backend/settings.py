10b0818f536e242b21d9ceeb2c48dc54bc623d2a
Ubuntu <ubuntu@ip-172-31-87-152.ec2.internal>
2023-07-07 10:23:24 +0000

    on server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@f*cs+m#hx@2nfd5gqvxiw7wh)-#o!l8sequadt^8^@=26rr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['44.212.59.103', 'techfest.org', 'rain.techfest.org']
+ALLOWED_HOSTS = ['44.212.59.103', 'techfest.org', 'rain.techfest.org', '*']
 
 
 # Application definition
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'apo/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'apo/')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
