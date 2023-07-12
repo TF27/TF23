5840ed09fd9538e69bdc14153b1f2ae9734168e6
CodeWithDebu <128091091+CodeWithDebu@users.noreply.github.com>
2023-07-12 17:51:56 +0530

    added the map component in the exhibition tab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar';
 import { AuthContextProvider } from './contexts/AuthContext';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home/home';
+import WorldMap from './pages/Exhibition/map';
 import Compi from './pages/Compi/compi';
 import Internal from './pages/Compi_Internal/internal';
 import Reg from './pages/Compi_Internal/Reg';
@@ -18,6 +19,7 @@ function App() {
                     <Navbar />
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/exhibitions' element={<WorldMap/>}/>
                         <Route path='/competitions' element={<Compi />} />
                         {/* <Route path={`${data.name}/register`} element={<Compi_Reg />} /> */}
                         {/* <Route path={`${data.name}`} element={<Compi_Int />} /> */}
