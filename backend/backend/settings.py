fc4aa2b102e889ae25eb3d49035bb181f2bf500a
Yat251 <yatharth85204@gmail.com>
2023-07-13 17:14:47 +0530

    updating

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-a@f*cs+m#hx@2nfd5gqvxiw7wh)-#o!l8sequadt^8^@=26rr6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['44.212.59.103', 'techfest.org', 'rain.techfest.org', '127.0.0.1', 'localhost']
 
 
@@ -145,8 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'apos/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'apo/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'apo/')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
