a3d120cf739f9b9271be31f7f4b41df400ae03bb
Yat251 <yatharth85204@gmail.com>
2023-08-11 10:10:23 +0530

    updated

---
+++ frontend/src/pages/gallery/specific/gallery_prop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from './courousel';
+
+const Gallery_prop = () => {
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+};
+
+export default Gallery_prop;
---
+++ frontend/src/pages/workshop/components/Card.js
@@ -0,0 +1,55 @@
+import React,{useState} from 'react';
+import cardview from './cardview.module.css'
+
+const Card = ({card}) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleLeave = () => {
+    setIsHovered(false);
+  };
+  return (
+    <div
+      onMouseEnter={handleHover}
+      onMouseLeave={handleLeave}
+    >
+    {isHovered ? 
+       <div className={cardview.outer}>
+            <div className={cardview.container}>
+               <div className={cardview.two}>
+                    <div className={cardview.wrapper}>
+                        <div className={cardview.overlay}>{card.desc}</div>
+                        <p>{card.name}</p>
+                    </div>
+                    <div className={cardview.buttonwrapper}> 
+                        <button className={`${cardview.btn} ${cardview.outline}`}> REGISTER </button>
+                        <button className={`${cardview.btn} ${cardview.outline}`}> EXPLORE </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    : 
+        <div className={cardview.outer}>
+            <div className={cardview.container}>
+               <div className={cardview.two}>
+                    <div className={cardview.wrapper}>
+                        <div className={cardview.bannerimage}></div>
+                        <p>{card.name}</p>
+                    </div>
+                    <div className={cardview.buttonwrapper}> 
+                        <button className={`${cardview.btn} ${cardview.outline}`}> REGISTER </button>
+                        <button className={`${cardview.btn} ${cardview.outline}`}> EXPLORE </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    }
+    </div>
+   )
+}
+
+export default Card
