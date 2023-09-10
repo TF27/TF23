9dd3230c8ca8de82a43440826bacfb1ba7980636
Prathamesh hatwar <yatharth85204@gmail.com>
2023-09-10 05:51:49 +0530

    carousel images added

--- frontend/src/pages/Compi/compi.jsx
+++ frontend/src/pages/Compi/compi.jsx
@@ -6,15 +6,34 @@ import { Frame3 } from "./compi_animation";
 import Technorion from "./technorion";
 import OurReach from "./ourreach";
 import Carousel from "../../components/Carousel/carousel";
-// import img from "../Lectures/assets/europe.jpg";
-// import img2 from "../Lectures/assets/lectbg.png";
 import "./compi_animation.css";
 import '../Lectures/lecture.css';
+import i1 from '../../pages/Exhibition/assets/i1.jpg'
+import i2 from '../../pages/Exhibition/assets/i2.jpg'
+import i3 from '../../pages/Exhibition/assets/i3.png'
+import i4 from '../../pages/Exhibition/assets/i4.jpg'
+import i5 from '../../pages/Exhibition/assets/i5.jpg'
+import i6 from '../../pages/Exhibition/assets/i6.jpg'
+import i7 from '../../pages/Exhibition/assets/i7.jpg'
+import i8 from '../../pages/Exhibition/assets/i8.jpg'
+import i9 from '../../pages/Exhibition/assets/i9.jpg'
+import i10 from '../../pages/Exhibition/assets/i10.jpg'
+
+import im1 from '../Lectures/assets/l1.jpg';
+import im2 from '../Lectures/assets/l2.jpg';
+import im3 from '../Lectures/assets/l3.jpg';
+import im4 from '../Lectures/assets/l4.jpg';
+import im5 from '../Lectures/assets/l5.jpg';
+import im6 from '../Lectures/assets/l6.JPG';
+import im7 from '../Lectures/assets/l7.jpg';
+import im8 from '../Lectures/assets/l8.jpg';
+import im9 from '../Lectures/assets/l9.jpg';
+import im10 from '../Lectures/assets/l10.jpg';
 
 const Compi = () => {
   const [data, setData] = useState([]);
   const { user } = UserAuth();
-
+  
   const google_id = user?.uid;
 
   useEffect(() => {
@@ -66,74 +85,55 @@ const Compi = () => {
       <OurReach />
       <br /><br /><br /><br />
       {/* {card()} */}
-       {/* <div className="checkThis">
+       <div className="checkThis">
         <h1 style={{ marginTop: "80px", marginBottom: "40px" }}>
           CHECK THIS OUT
         </h1>
         <div>
-          <h2 style={{ marginBottom: "-20px" }}>Exhibitions</h2>
+          <h2 style={{ marginBottom: "-20px" }}>Competitions</h2>
           <Carousel
-            i1={img}
-            i2={img2}
-            i3={img}
-            i4={img2}
-            i5={img}
-            i8={img2}
-            i6={img}
-            i7={img2}
-            i9={img}
-            i10={img2}
+            i1={i1}
+            i2={i2}
+            i3={i3}
+            i4={i4}
+            i5={i5}
+            i8={i8}
+            i6={i6}
+            i7={i7}
+            i9={i9}
+            i10={i10}
           />
           <div className="difPage">
             <div className="check_rect1"></div>
             <div className="check_rect2">
-              <Link to="/exhibitions">Explore</Link>
+              <Link to="/exhibitions">Exhibitions</Link>
             </div>
           </div>
+          <br /><br /><br /><br /><br /><br /><br /><br />
         </div>
         <div style={{marginBottom: '150px'}}>
           <h2 style={{ marginTop: "40px", marginBottom: "-20px" }}>Lectures</h2>
           <Carousel
-            i1={img}
-            i2={img2}
-            i3={img}
-            i4={img2}
-            i5={img}
-            i8={img2}
-            i6={img}
-            i7={img2}
-            i9={img}
-            i10={img2}
+            i1={im1}
+            i2={im2}
+            i3={im3}
+            i4={im4}
+            i5={im5}
+            i8={im8}
+            i6={im6}
+            i7={im7}
+            i9={im9}
+            i10={im10}
           />
           <div className="difPage">
             <div className="check_rect1"></div>
             <div className="check_rect2">
-              <Link to="/lectures">Explore</Link>
+              <Link to="/lectures">Lectures</Link>
             </div>
           </div>
         </div>
-      </div> */}
-      {/* <div className="contactus">
-        <h1 style={{ marginTop: "80px" }}>CONTACT US</h1>
-        <div className="tray">
-          <div>
-            <ContactCard
-              image=""
-              headText="Contact Us"
-              emailText="contact@example.com"
-              phoneNumberText="123-456-7890"
-            />
-          </div>
-          <div>
-            <ContactCard
-              image=""
-              headText="Contact Us"
-              emailText="contact@example.com"
-              phoneNumberText="123-456-7890"
-            />
-          </div>
-        </div>
-      </div>  */}
+      </div>
+      
     </div>
   );
 };
