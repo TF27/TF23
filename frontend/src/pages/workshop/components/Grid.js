0ed01b184378e58409e1577b7bab8ed145fc8819
Yatharth <yatharth85204@gmail.com>
2023-10-07 17:48:30 +0530

    workshops

--- frontend/src/pages/workshop/components/Grid.js
+++ frontend/src/pages/workshop/components/Grid.js
@@ -2,18 +2,15 @@ import React from 'react';
 import Card from './Card1';
 
 const Grid = ({cards}) => {
+
+  const sortedCards = cards.slice().sort((a, b) => b.workshop_id - a.workshop_id);
   
   return (
-    // <div className={`${grid.grid} ${grid.container}`}>
-    //   {cards.map((card)=>(
-    //    <Card  key={card.index} card={card} />
-    //    ))}
-    // </div>
     <>
       <div className="technorion_cards">
         <div className="container" style={{ maxWidth: "90%" }}>
           <div className="row">
-            {cards.map((card) => (
+          {sortedCards.map((card) => (
               <Card key={card.index} card={card} />
             ))}
           </div>
