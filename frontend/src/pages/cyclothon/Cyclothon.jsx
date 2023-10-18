f89efacac93fcd2278b9a42b421a2624c918432c
praveen-prajapat <128595854+praveenprajapat2004@users.noreply.github.com>
2023-10-18 15:32:41 +0530

    cyclothon

--- frontend/src/pages/cyclothon/Cyclothon.jsx
+++ frontend/src/pages/cyclothon/Cyclothon.jsx
@@ -5,6 +5,7 @@ import './home.css';
 import triangle from './img/triangle.png'
 import cyclo from './img/ABOUT US.png'
 import circle from './img/circle.png';
+import food from './img/food.png'
 import vision from './img/vision title.png';
 import incentives from './img/incentives title.png'
 import incentives1 from './img/incentives1.png'
@@ -12,6 +13,7 @@ import incentives2 from './img/incentives2.png'
 import incentives3 from './img/incentives3.png'
 import layer6 from './img/layer6.png';
 import sponsor from './img/sponsor.png'
+import decathlon from './img/decathlon.png'
 import contact from './img/contact.png'
 import schedule from './img/schedule-title.png'
 
@@ -21,6 +23,9 @@ const Cyclothon = () => {
 
   const redirectToExternalSite = () => {
     window.location.href = 'https://www.techfest.org'; 
+  };
+  const rentcycle = () => {
+    window.location.href = 'https://www.meraevents.com/event/cycle-on-rent-cyclothon-2023-iitb?ucode=organizer'; 
   };
 
   useEffect(() => {
@@ -76,12 +81,12 @@ const Cyclothon = () => {
     <div className={`${parallax.parallaxlayer} ${parallax.layer3}`} />
     <div className={parallax.blackGradientbottom} style={{ transform: `translateY(${scrollPosition * 0}px)`,scrollBehavior:'smooth' }}>
     <btn className={parallax.custombutton1} onClick={redirectToExternalSite}>
-      RENT A CYCLE
-    </btn>
-    <btn className={parallax.custombutton2} onClick={redirectToExternalSite}>
       REGISTER NOW
     </btn>
-    <h3 style={{position:'relative',font:'Montserrat'}} className={parallax.date}>15 OCTOBER |  6 AM | IIT BOMBAY, POWAI </h3>
+    <btn className={parallax.custombutton2} onClick={rentcycle}>
+      RENT A CYCLE
+    </btn>
+    <h3 style={{position:'relative',font:'Montserrat'}} className={parallax.date}>5 NOVEMBER | 6 AM | IIT BOMBAY, POWAI</h3>
     </div>
   </div>
     <div className={parallax.image} >
@@ -92,34 +97,44 @@ const Cyclothon = () => {
         </div>
         <div className="col-md-6 class2">
           <img src={cyclo} alt="sorry no image" className="img-fluid" style={{maxWidth:'70%',marginBottom:'15px'}} />
-          <p className="visiontext">Techfest, IIT Bombay invites you to the 4th edition of the Techfest Cyclothon. Experience the serenity of nature in the heart of the urban jungle as you pedal across a 12 km long route that will provide you with a glimpse of the natural beauty of the lush, green IIT Bombay campus.Join us on the 6th of October in pledging to include this eco-friendly mode of transport in our daily lives and take the first step towards stopping air pollution with the Techfest Cyclothon!</p>
+          <p className="visiontext">Techfest, IIT Bombay is thrilled to present the 6th edition of the Techfest Cyclothon with a profound focus on sustainability. On the 5th of November, we invite you to partake in a memorable journey that beautifully marries nature and urban living. As you cycle through a captivating 15 km route that winds through the lush IIT Bombay campus, we aim to kindle a sense of eco-consciousness and the significance of sustainable practices in our daily lives. Together, let's pedal towards a cleaner, greener future at the Techfest Cyclothon.
+</p>
         </div>
       </div>
       <img src={vision} alt="sorry no image"  className="img-fluid vision" style={{maxWidth:'50%',marginTop:'50px'}} />
       <div>
-        <p className="visiontext">With a vision to promote cycling as a green and eco-friendly means of transport, the Techfest Cyclothon aims to help you take the first step in the fight against air pollution by adopting the humble bicycle as the perfect alternative to fuel guzzlers in cities like Mumbai.</p>
+        <p className="visiontext">We envision a world where every pedal stroke is a step towards a greener, cleaner planet. Our vision is to inspire a sustainable revolution, where the simple act of cycling becomes a powerful catalyst for positive change. Join us in this movement, as we pedal together to reduce our carbon footprint, protect the environment, and pave the way for a brighter, more sustainable future. Let's make a difference, one pedal at a time.
+</p>
       </div>
       <img src={incentives} alt="sorry no image"  className="img-fluid" style={{maxWidth:'80%',marginTop:'50px'}} />
-      <div className="container">
-      <div className="row">
-        <div className="col-md-4">
+      <div className="container1">
+      <div className="row1">
+        <div className="col-md-3 element1" >
           <div className="flex-item" style={{display:'flex',flexDirection:'rows',marginBottom:'-38px'}}>
             <img src={incentives1} alt="sorry no image"  className="img-fluid" style={{maxWidth:'16%',marginTop:'35px'}} />
             <p className="visiontext" style={{marginTop:'65px'}}>Free Entry</p>
           </div>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 element2" >
+          <div className="flex-item" style={{display:'flex',flexDirection:'rows'}}>
+            <img src={food} alt="sorry no image"  className="img-fluid" style={{maxWidth:'14%',marginTop:'49px'}} />
+            <p className="visiontext" style={{marginTop:'65px',marginLeft:'10px'}}>Free FnB</p>
+          </div>
+        </div>
+        
+        <div className="col-md-3 element3" >
+          <div className="flex-item" style={{display:'flex',flexDirection:'rows'}}>
+            <img src={incentives3} alt="sorry no image"  className="img-fluid" style={{maxWidth:'17%',marginTop:'39px'}} />
+            <p className="visiontext" style={{marginTop:'65px',marginLeft:'10px'}}>Free Fitness Session</p>
+          </div>
+        </div>
+        <div className="col-md-3 element4" >
           <div className="flex-item" style={{display:'flex',flexDirection:'rows',marginBottom:'-38px'}}>
             <img src={incentives2} alt="sorry no image"  className="img-fluid" style={{maxWidth:'17%',marginTop:'39px'}} />
             <p className="visiontext" style={{marginTop:'65px'}}>Free Cyclothon T-shirts</p>
           </div>
         </div>
-        <div className="col-md-4">
-          <div className="flex-item" style={{display:'flex',flexDirection:'rows'}}>
-            <img src={incentives3} alt="sorry no image"  className="img-fluid" style={{maxWidth:'17%',marginTop:'39px'}} />
-            <p className="visiontext" style={{marginTop:'65px'}}>Free Fitness Session</p>
-          </div>
-        </div>
+        
       </div>
     </div>
     </div>
@@ -130,24 +145,44 @@ const Cyclothon = () => {
     </div>
   
     <img src={sponsor} alt="sorry no image" className="img-fluid sponser" />
+    <p></p>
+    <img src={decathlon} alt="sorry no image" className="img-fluid decathlon" />
 
-     <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <div className="text-center">
-          <h3  className={parallax.sponser1}>LOREM IPSUM</h3>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="text-center">
-          <h3  className={parallax.sponser2}>LOREM EPSUM</h3>
-          </div>
-        </div>
+     
+    
+      <div>
+        <p className="tnc">TERMS & CONDITIONS</p>
       </div>
-    </div>
+      <div>
+        <p className="tnctext">1. Those who did not have cycle and still want to participate in cyclothon, should click on "Rent a cycle" above.<br/>
+
+2. This is a Cyclothon event and not a race. The road closure is for a few km distances and a few crucial points only. So every participant is expected to take care of their safety at their own risk and riders have to follow all the rules.<br/>
+
+3. Though the event organizer will provide all support of Hydration, Medical, Technician, and Crew support, it is expected that if participants have any prior medical condition, they have to take suggestions from their family doctor about participation.<br/>
+
+4. Electric Bicycle is not allowed; they will be disqualified if any participant is found.<br/>
+
+5. Food and Beverages will be distributed at the end of the event<br/>
+
+6. In case of any theft or misplace of your product, Techfest will not be responsible.<br/> 
+
+9. In case of the event's date changes, the participant can claim the full refund at <a href="techfest.org/cyclothon">techfest.org/cyclothon</a> or existing registration will be carried forward to the new date.<br/>
+
+10. If the event gets cancelled due to unavoidable circumstances, the participant will get a full refund.<br/>
+
+11. The organizer has the right to change the venue, Route, and Event time as per the IITB Guidelines or Permission.<br/>
+
+12. In case of any change in Event time, Venue and Route, we will notify you through email or the event website <a href="techfest.org">techfest.org</a> For any queries, you can email us at <a href="manager.techfest@gmail.com">manager.techfest@gmail.com</a><br/>
+
+13. Techfest will provide the Services to the Participants of The Event up until its completion free of cost to the Participants. For any medical care further to the Services provided to the Participant and for which admission into any in-patient department (IPD) is required, the Participant shall be admitted to the nearest hospital and the Participant or relative shall follow the policies of that hospital for admission and billing. In case the Participant is admitted to the Hospital, he/ she shall be charged for the medical services availed there as per the prevailing policy of that hospital and the Participant shall bear such costs.</p>
+      </div>
     <img src={contact} alt="sorry no image" className="img-fluid contact"  />
-    <h3 style={{position:'relative',top:'10vh',font:'montserrat'}} className={parallax.date}>LOREM EPSUM</h3>
-    <h3 style={{position:'relative',top:'10vh',font:'montserrat',marginBottom:'10vh'}} className={parallax.date}>LOREM EPSUM</h3>
+    <h3 style={{position:'relative',top:'10vh',font:'montserrat',marginBottom:'5vh'}} className={parallax.date}>Ajay Agrawal<br/>
++91 7509759232
+
+</h3>
+    <h3 style={{position:'relative',top:'10vh',font:'montserrat',marginBottom:'20vh'}} className={parallax.date}>Prathamesh Hatwar<br/>
++91 7387041365 </h3>
  </div>
   )
 };
