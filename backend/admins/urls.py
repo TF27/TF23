6837889e4154530b7c9ec926105319267b684668
Yatharth <yatharth85204@gmail.com>
2023-10-20 16:00:28 +0530

    admin portal

--- backend/admins/urls.py
+++ backend/admins/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<str:compiname>', views.compi, name='compi'),
     path('export-csv-all/', views.export_data_to_csv_all, name='export-csv-all'),
     path('export-compi-team-csv/', views.download_compi_team_csv, name='export-compi-team-csv'),
+    path('download_csv_ofCompi/<str:compiname>/', views.download_csv_ofCompi, name='download_csv_ofCompi'),
+    path('download_csv_ofCompiTeam/<str:compiname>/', views.download_csv_ofCompiTeam, name='download_csv_ofCompiTeam'),
 ]
