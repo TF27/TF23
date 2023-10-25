9d269a2e2416f93990b602e0ec70356b9e657173
ATHARV APSHINGE <yatharth85204@gmail.com>
2023-10-25 12:18:32 +0530

    updating

--- frontend/src/pages/workshop/internal/Workshops/Auto.jsx
+++ frontend/src/pages/workshop/internal/Workshops/Auto.jsx
@@ -103,7 +103,7 @@ const Auto = () => {
                 <div className={styles.info_details}>
                   <div>
                     <h3>Date: </h3>
-                    <p>26th-27th December</p>
+                    <p>28th-29th December</p>
                     <h3>Venue: </h3>
                     <p>IIT Bombay</p>
                     <h3>
