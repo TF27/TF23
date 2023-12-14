7a0542c53b05dc1c70b4d161c15b6d640949d2fa
CodeWithDebu <128091091+CodeWithDebu@users.noreply.github.com>
2023-12-14 23:38:26 +0530

    upar ke links daal diye

--- frontend/src/pages/AboutUs/Sponsors/sponsors.jsx
+++ frontend/src/pages/AboutUs/Sponsors/sponsors.jsx
@@ -55,7 +55,7 @@ import prep from './assets/Campus Ambassador Partnrs/Prepp in.png';
 import udemy from './assets/Campus Ambassador Partnrs/Udemy.png';
 import uiux from './assets/Campus Ambassador Partnrs/UI UX Global.jpg';
 import cnbc from './assets/media/cnbc.jpeg';
-import cinepolis from './assets/media/cnbc.jpeg';
+import cinepolis from './assets/cine.jpeg';
 import jio from './assets/media/Jio Saavn Logo.png';
 import maha from './assets/media/maha.jpeg';
 import ontv from './assets/media/ONTV LOGO.png';
@@ -73,7 +73,7 @@ import toi from './assets/media/toi.jpg';
 import ufo from './assets/UFO logo outdoor.png';
 import youth from './assets/media/youth.png';
 import britishcouncil from './assets/BritishCouncil.jpeg';
-import gaana from './assets/media/toi.jpg';
+import gaana from './assets/Gaana_Logo.png';
 import timetechies from './assets/media/timetechies.png';
 
 
@@ -109,49 +109,93 @@ const Sponsors = () => {
     <div className='yoyohoneysingh' style={bgHaiJi}>
       <div className={styles.head}><h1>Title Sponsor</h1></div>
       <div className={styles.singlehead}>
+        <a href='https://www.marshmclennan.com/'>
         <img src={mars}></img>
+        </a>
       </div>
       <div className={styles.title}><h1>Marsh McLennan Global Services India Pvt Ltd</h1></div>
       <div className={styles.title}><h1>Associate Title Sponsor</h1></div>
       <div className={styles.bobby}>
+        <a href='https://www.sbi.co.in/web/home/home'>
         <img src={sbi}></img>
+        </a>
       </div>
       <div className={styles.title}><h1>Powered By</h1></div>
       <div className={styles.bobby}>
+        <a href='http://here.com/'>
         <img src={here}></img>
+        </a>
+        <a href='http://idfcfirstbank.com/'>
         <img src={idfc}></img>
+        </a>
+        <a href='http://unstop.com/'>
         <img src={unstop}></img>
+        </a>
       </div>
       <div className={styles.title}><h1>Competitions Sponsor</h1></div>
       <div className={styles.bobby}>
+        <a href='http://findoc.com/'>
         <img src={findoc}></img>
+        </a>
+        <a href='http://boeing.com/'>
         <img src={boeing}></img>
+        </a>
+        <a href='http://atlassian.com/'>
         <img src={atlassian}></img>
+        </a>
       </div>
       <div className={styles.bobby}>
+        <a href='http://datamatics.com/'>
         <img src={datamatics}></img>
+        </a>
+        <a href='http://tih.iitb.ac.in/'>
         <img src={tih}></img>
+        </a>
+        <a href='http://fetch.ai/'>
         <img src={fetch}></img>
+        </a>
       </div>
       <div className={styles.bobby}>
+      <a href='https://www.edelweisstokio.in/'>
         <img src={edel}></img>
+        </a>
+        <a href='http://airbus.com/'>
         <img src={airbus}></img>
+        </a>
+        <a href='http://atomberg.com/'>
         <img src={atomberg}></img>
+        </a>
       </div>
       <div className={styles.bobby}>
+      
         <img src={olympiad}></img>
+        
+        <a href='http://witblox.com/'>
         <img src={witblox}></img>
+        </a>
+        <a href='https://techradiance.in/'>
         <img src={techrad}></img>
+        </a>
       </div>
       <div className={styles.bobby}>
+      <a href='https://ableducation.com/'>
         <img src={abl}></img>
+        </a>
+        
         <img src={prag}></img>
+        
+        <a href='https://www.robocapleague.com/'>
         <img src={robocap}></img>
+        </a>
       </div>
       <div className={styles.title}><h2>Innovation Partner</h2><h2> Official Tech-Aid Partner</h2></div>
       <div className={styles.bobby}>
+        <a href='http://godrej.com/'>
         <img src={godrej}></img>
+        </a>
+        <a href='https://csi.thenudge.org/'>
         <img src={nudge}></img>
+        </a>
       </div>
       <div className={styles.title}><h1>International Summit Partners</h1></div>
       <div className={styles.bobby2}>
