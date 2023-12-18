9b538e198ad00cf8970540ef7f828f05e2fb7c0f
Yatharth <yatharth85204@gmail.com>
2023-12-18 22:17:43 +0530

    workshop

--- frontend/src/pages/workshop/components/Grid.js
+++ frontend/src/pages/workshop/components/Grid.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Card from './Card1';
+import styles from './styles.module.css';
+import head from '../img/workshop_header.png';
 
 const Grid = ({cards}) => {
 
@@ -8,6 +10,9 @@ const Grid = ({cards}) => {
   return (
     <>
       <div className="technorion_cards">
+        <div className={styles.head}>
+          <img src={head} alt="header"/>
+        </div>
         <h2 style={{marginTop: '15px'}}>Team discount and Early bird offer extended till 18th December!</h2>
         <div className="container" style={{ maxWidth: "90%" }}>
           <div className="row" style={{paddingBottom: '80px'}}>
